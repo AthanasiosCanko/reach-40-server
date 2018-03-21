b507e99433fb19e85a08b5ebac45cea6517da352
Athanasios <cankoathanasios@gmail.com>
2018-03-21 11:42:51 +0100

    Syntactic sugar on json responses

--- index.js
+++ index.js
@@ -21,20 +21,20 @@ app.get("/:name/:score", function(req, res) {
     if ((typeof name == "string") && (typeof score == "number")) {
         high_score.findOne({name: name}, function(err, item) {
 
-            if (err) res.json({"message": err})
+            if (err) res.json({"message": "Server error, please try later."})
             else if (!item) {
                 high_score.create({name: name, score: score}, function(err, item) {
 
-                    if (err || !item) res.json({"message": "error"})
-                    else res.json({name: item.name,score: item.score,message: "success"})
+                    if (err || !item) res.json({"message": "Database error, please try later."})
+                    else res.json({name: item.name,score: item.score,message: "Success, score saved."})
 
                 })
             }
-            else res.json({"message": "username taken"})
+            else res.json({"message": "Sorry, username taken."})
 
         })
     }
-    else res.json({"message": "error"})
+    else res.json({"message": "URL error, please try again."})
 })
 
 app.get("/all", function(req, res) {
